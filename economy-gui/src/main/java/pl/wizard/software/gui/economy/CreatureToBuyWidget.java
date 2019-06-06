e44a5e34ccbf7d24d3296eae5200b83bd6b852c5
software-wizard <przemek@software-wizard.pl>
2019-06-06 05:37:52 +0200

    upgrade view

--- economy-gui/src/main/java/pl/wizard/software/gui/economy/CreatureToBuyWidget.java
+++ economy-gui/src/main/java/pl/wizard/software/gui/economy/CreatureToBuyWidget.java
@@ -15,7 +15,22 @@ class CreatureToBuyWidget extends HBox {
         Slider slider = new Slider();
         slider.setMax(10);
         slider.setMin(0);
+        slider.setBlockIncrement(1);
+        slider.setMajorTickUnit(1);
+        slider.setMinorTickCount(0);
+        slider.setShowTickLabels(true);
+        slider.setSnapToTicks(true);
         getChildren().add(slider);
+        Label creatureCount = new Label("0");
+        getChildren().add(new Label("Creature count: "));
+        getChildren().add(creatureCount);
+        Label goldCost = new Label("0");
+        getChildren().add(new Label(" Creature cost: "));
+        getChildren().add(goldCost);
+        slider.valueProperty().addListener((aObs, aOld, aNew) ->{
+            creatureCount.setText(String.valueOf(aNew.intValue()));
+            goldCost.setText(String.valueOf(aNew.intValue() * creature.getGoldCost()));
+        });
     }
 
 
--- economy-model/src/main/java/pl/wizard/software/economy/CreatureEcoDomain.java
+++ economy-model/src/main/java/pl/wizard/software/economy/CreatureEcoDomain.java
@@ -33,4 +33,8 @@ public class CreatureEcoDomain {
     public String getName() {
         return name;
     }
+
+    public int getGoldCost() {
+        return goldCost;
+    }
 }
